fe0311b90bdd84bc4fd43525fca146967014ab8c
Rimve <rimvydas.neverauskas@ktu.edu>
2021-03-02 22:48:38 +0200

    Implemented context menu functions - add (works with database), remove (works only visually for now)

--- DialogueManager/src/components/GraphComponent.js
+++ DialogueManager/src/components/GraphComponent.js
@@ -3,7 +3,7 @@ import cytoscape from 'cytoscape';
 import cxtmenu from 'cytoscape-cxtmenu';
 import '../styles/CyStyle.css';
 import {withRouter} from "react-router-dom";
-import {getCharAdjRef, getCharNodeRef} from "../services/DatabaseService";
+import {dbUpdateAdjacencies, dbUpdateNodes, getCharAdjRef, getCharNodeRef} from "../services/DatabaseService";
 import "firebase/database";
 
 class GraphComponent extends React.Component{
@@ -14,7 +14,7 @@ class GraphComponent extends React.Component{
         this.state = {
             nodes: [],
             edges: [],
-            data: [],
+            name: "",
             update: false
         };
     }
@@ -26,44 +26,53 @@ class GraphComponent extends React.Component{
         }
 
         let defaults = {
-            menuRadius: function(ele){ return 50; }, // the outer radius (node center to the end of the menu) in pixels. It is added to the rendered size of the node. Can either be a number or function as in the example.
-            selector: 'node', // elements matching this Cytoscape.js selector will trigger cxtmenus
+            menuRadius: function(ele){ return 50; },
+            selector: 'node',
             commands: [
                 {
-                    fillColor: 'rgba(222,4,0,0.5)', // optional: custom background color for item
+                    fillColor: 'rgba(222,4,0,0.6)',
                     opacity: 0.1,
-                    content: 'Remove', // html/text content to be displayed in the menu
-                    contentStyle: {}, // css key:value pairs to set the command's css in js if you want
-                    select: function(ele){ // a function to execute when the command is selected
-                        console.log(ele.id()) // `ele` holds the reference to the active element
+                    content: 'Remove',
+                    contentStyle: {},
+                    select: (ele) => {
+                        this.removeNode(ele.data().scratch);
                     },
-                    enabled: true // whether the command is selectable
+                    enabled: true
                 },
                 {
-                    fillColor: 'rgba(222,4,0,0.5)',
+                    fillColor: 'rgba(222,4,0,0.6)',
                     content: 'Edit',
                     contentStyle: {},
-                    select: function(ele){
-                        console.log(ele.id())
+                    select: function(ele) {
+                        console.log(ele.data())
+                    },
+                    enabled: true
+                },
+                {
+                    fillColor: 'rgba(222,4,0,0.6)',
+                    content: 'Add',
+                    contentStyle: {},
+                    select: (ele) => {
+                        this.addNode(ele.data());
                     },
                     enabled: true
                 }
             ],
-            fillColor: 'rgba(0, 0, 0, 0.75)', // the background colour of the menu
-            activeFillColor: 'rgba(59,82,86,0.5)', // the colour used to indicate the selected command
-            activePadding: 20, // additional size in pixels for the active command
-            indicatorSize: 12, // the size in pixels of the pointer to the active command, will default to the node size if the node size is smaller than the indicator size,
-            separatorWidth: 5, // the empty spacing in pixels between successive commands
-            spotlightPadding: 5, // extra spacing in pixels between the element and the spotlight
-            adaptativeNodeSpotlightRadius: false, // specify whether the spotlight radius should adapt to the node size
-            minSpotlightRadius: 24, // the minimum radius in pixels of the spotlight (ignored for the node if adaptativeNodeSpotlightRadius is enabled but still used for the edge & background)
-            maxSpotlightRadius: 38, // the maximum radius in pixels of the spotlight (ignored for the node if adaptativeNodeSpotlightRadius is enabled but still used for the edge & background)
-            openMenuEvents: 'cxttapstart taphold', // space-separated cytoscape events that will open the menu; only `cxttapstart` and/or `taphold` work here
-            itemColor: 'white', // the colour of text in the command's content
-            itemTextShadowColor: 'transparent', // the text shadow colour of the command's content
-            zIndex: 9999, // the z-index of the ui div
-            atMouse: false, // draw menu at mouse position
-            outsideMenuCancel: false // if set to a number, this will cancel the command if the pointer is released outside of the spotlight, padded by the number given
+            fillColor: 'rgba(0, 0, 0, 0.75)',
+            activeFillColor: 'rgba(59,82,86,0.5)',
+            activePadding: 20,
+            indicatorSize: 12,
+            separatorWidth: 5,
+            spotlightPadding: 5,
+            adaptativeNodeSpotlightRadius: false,
+            minSpotlightRadius: 24,
+            maxSpotlightRadius: 38,
+            openMenuEvents: 'cxttapstart taphold',
+            itemColor: 'white',
+            itemTextShadowColor: 'transparent',
+            zIndex: 9999,
+            atMouse: false,
+            outsideMenuCancel: false
         };
 
         let cy = cytoscape(
@@ -100,10 +109,13 @@ class GraphComponent extends React.Component{
                         'curve-style': 'bezier'
                     })
                 ,
-                elements: this.state.data,
+                elements: {
+                    nodes: this.state.nodes,
+                    edges: this.state.edges
+                },
 
                 layout: {
-                    name: 'breadthfirst',
+                    name: 'circle',
                     directed: true,
                     padding: 20,
                     avoidOverlap: true,
@@ -116,40 +128,129 @@ class GraphComponent extends React.Component{
 
     getData(name) {
         let nodes = [];
-        getCharNodeRef(name)
-            .on('value', (snapshot) => {
-                let results = snapshot.val();
-                for (let index in results) {
-                    const node = {
-                        "data": {
-                            id: results[index]
-                        }
-                    };
-                    nodes.push(node);
-                }
-            });
+        let edges = [];
 
-        getCharAdjRef(name)
-            .on('value', (snapshot) => {
-                let results = snapshot.val();
-                for (let index in results) {
-                    let srcIndex = results.indexOf(results[index]);
-                    results[index].map((data, index) => {
+        getCharNodeRef(name).on('value', (snapshot) => {
+            let results = snapshot.val();
+            for (let index in results) {
+                const node = {
+                    "data": {
+                        id: results[index],
+                        scratch: results.indexOf(results[index])
+                    }
+                };
+                nodes.push(node);
+            }
+        });
+
+        getCharAdjRef(name).on('value', (snapshot) => {
+            let results = snapshot.val();
+            for (let index in results) {
+                let resultObj = Object.values(results[index]);
+                resultObj.map((data, x) => {
+                    if (typeof (nodes[data]) !== 'undefined') {
                         const edge = {
                             "data": {
-                                source: nodes[srcIndex].data.id,
+                                source: nodes[index].data.id,
                                 target: nodes[data].data.id
                             }
                         };
-                        nodes.push(edge);
-                    })
-                }
-                this.setState({data: nodes});
+                        edges.push(edge);
+                    }
+                })
+
+            }
+
+            this.setState({
+                edges: edges,
+                nodes: nodes,
+                update: true
             });
+        });
+    }
+
+    getIdOfLastNode() {
+        const {nodes} = this.state;
+        return nodes[nodes.length - 1].data.scratch
+    }
+
+    getNodesArraySize(srcNode) {
+        const {edges} = this.state;
+        return edges.filter(edge => edge.data.source === srcNode.id).length - 1;
+    }
+
+    addNode(selectedNode) {
+        const {nodes} = this.state;
+        const {edges} = this.state;
+        const {name} = this.state;
+
+        let lastId = this.getIdOfLastNode() + 1;
+        const node = {
+            "data": {
+                id: "empty" + lastId.toString(),
+                scratch: lastId
+            }
+        };
+        nodes.push(node);
+
+        const edge = {
+            "data": {
+                source: nodes[selectedNode.scratch].data.id,
+                target: nodes[lastId].data.id
+            }
+        };
+        edges.push(edge);
+
+        this.setState({
+            nodes: nodes,
+            edges: edges,
+            update: false
+        });
+
+        this.addNodeToDatabase(name, node);
+        this.addAdjToDatabase(name, nodes[selectedNode.scratch].data, nodes[lastId].data);
+    }
+
+    removeNode(id) {
+        let nodeToDelete = '';
+        let nodes = this.state.nodes.filter(function(node) {
+            if (node.data.scratch !== id) {
+                return node;
+            }
+            else {
+                nodeToDelete = node;
+            }
+        });
+
+        let edges = this.state.edges.filter(function(edge) {
+            if (edge.data.target === nodeToDelete.data.id ||
+                edge.data.source === nodeToDelete.data.id) {
+
+            }
+            else {
+                return edge;
+            }
+        });
+
+        this.setState({
+            nodes: nodes,
+            edges: edges,
+            update: false
+        });
+    }
+
+    addNodeToDatabase(name, node) {
+        dbUpdateNodes(name, [node.data.scratch]).set(node.data.id);
+    }
+
+    addAdjToDatabase(name, source, target) {
+        let index = this.getNodesArraySize(source);
+        dbUpdateAdjacencies(name, source.scratch, index).set(target.scratch);
     }
 
     componentDidMount() {
         if (typeof(this.props.location) !== 'undefined' && this.props.location != null) {
+            this.setState({name: this.props.location.state.name});
             this.getData(this.props.location.state.name);
         }
         else {
@@ -158,8 +259,12 @@ class GraphComponent extends React.Component{
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        if (prevState.data !== this.state.data) {
+        if (prevState.edges !== this.state.edges ||
+            prevState.update !== this.state.update) {
             this.renderCytoscapeElement();
+            this.setState({update: true});
+            // console.log(this.state.nodes);
+            // console.log(this.state.edges);
         }
     }
 
