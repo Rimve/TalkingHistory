858376a8adc3b878284d3675bf9a0da5ef09ddd7
Rimve <rimvydas.neverauskas@ktu.edu>
2021-03-10 15:14:55 +0200

    Removed all of the commented code

--- app/src/main/java/com/neverim/talkinghistory/ui/viewmodels/RecognizerViewModel.kt
+++ app/src/main/java/com/neverim/talkinghistory/ui/viewmodels/RecognizerViewModel.kt
@@ -5,16 +5,11 @@ import androidx.lifecycle.ViewModel
 import com.neverim.talkinghistory.data.repositories.RecognizerRepository
 
 class RecognizerViewModel(private val recognizerRepo: RecognizerRepository, private val context: Context) : ViewModel() {
-//    suspend fun getTranscript() = recognizerRepo.getTranscript()
-//    fun audioSetup() = recognizerRepo.audioSetup()
-//    fun recordAudio() = recognizerRepo.recordAudio()
-//    fun stopAudio() = recognizerRepo.stopAudio()
-//    fun isRecording() = recognizerRepo.isRecording()
-
 
     fun audioSetup() = recognizerRepo.audioSetup(context)
     fun startRecognition() = recognizerRepo.startRecognition(context)
     fun stopRecognition() = recognizerRepo.stopRecognition()
     fun isRecognizing() = recognizerRepo.isRecognizing()
     fun getTranscript() = recognizerRepo.getTranscript()
+
 }
